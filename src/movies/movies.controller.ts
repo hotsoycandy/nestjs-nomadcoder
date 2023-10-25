9a945e62ea8c83b098f37bdf71efd207eac6590d
hotsoycandy <wkdwnsghd617@gmail.com>
2023-10-25 23:16:30 +0900

    movies.controller - add movies crud routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  @Get()
+  getAll(): string {
+    return 'This will return all movies';
+  }
+
+  @Get('/:movieId')
+  getOne(@Param('movieId') movieId: string): string {
+    return `This will return a movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create(): string {
+    return 'This will create movie';
+  }
+
+  @Delete('/:movieId')
+  remove(@Param('movieId') movieId: string): string {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:movieId')
+  patch(@Param('movieId') movieId: string): string {
+    return `This will patch a movie with the id: ${movieId}`;
+  }
+}
